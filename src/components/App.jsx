9e04d73a32259a6a8b6a781616c704186fe929ee
Garrett Pfingston <gpfingston@gmail.com>
2025-11-05 17:20:01 -0600

    added form logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,17 +4,20 @@ import Header from "./Header";
 import itemData from "../data/items";
 
 function App() {
-  const [items, setItems] = useState(itemData);
+  const [items, setItem] = useState(itemData);
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
+  function handleNewItem(newItem) {
+    setItem((prevItems) => [...prevItems, newItem.item]);
+  }
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList items={items} handleNewItem={handleNewItem} />
     </div>
   );
 }
--- src/components/ItemForm.jsx
+++ src/components/ItemForm.jsx
@@ -1,17 +1,44 @@
 import React from "react";
 import { v4 as uuid } from "uuid";
+import { useState } from "react";
 
-function ItemForm(props) {
+function ItemForm({ items, handleNewItem, onItemFormSubmit }) {
+  const [item, setItemName] = useState({ name: "", category: "Produce" });
+  
+  function handleChange(event) {
+    setItemName({ ...item, [event.target.name]: event.target.value });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const newItem = { ...item, id: uuid() };
+
+    // add check for tests
+    if (typeof onItemFormSubmit === "function") {
+      onItemFormSubmit(newItem);
+    } else if (typeof handleNewItem === "function") {
+      handleNewItem({ item: newItem });
+    }
+    setItemName({ name: "", category: "Produce" });
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input
+          type="text"
+          name="name"
+          value={item.name}
+          onChange={handleChange}
+          placeholder="Add Item"
+        />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" value={item.category} onChange={handleChange}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
